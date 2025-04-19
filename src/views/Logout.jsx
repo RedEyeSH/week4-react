27d079f19db72aa43c99580303952f70d0167fdf
RedEyeSH <97534002+RedEyeSH@users.noreply.github.com>
2025-04-19 14:58:07 +0300

    Implement user authentication: add UserContext, ProtectedRoute, and update routing; enhance Login and Logout components

--- src/views/Logout.jsx
+++ src/views/Logout.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import { useUserContext } from "../hooks/contextHooks";
+
 const Logout = () => {
+    const { handleLogout } = useUserContext();
+    
+    useEffect(() => {
+        handleLogout();
+    }, []);
+
     return <p>Logout page here</p>;
-  };
+};
   
-  export default Logout;
+export default Logout;
